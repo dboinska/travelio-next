d865e0401eda0d1d7478dcfdf8a8dae824866ab3
Dorota Boinska <dboinska@gmail.com>
2026-05-23 11:34:25 +0200

    feat(db): add Prisma configuration and MongoDB schema

---
+++ lib/auth.ts
@@ -0,0 +1,51 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+
+import { prisma } from "@/lib/prisma";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  session: {
+    strategy: "jwt",
+  },
+
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          return null;
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email as string,
+          },
+        });
+
+        if (!user) {
+          return null;
+        }
+
+        const passwordsMatch = await bcrypt.compare(
+          credentials.password as string,
+          user.password
+        );
+
+        if (!passwordsMatch) {
+          return null;
+        }
+
+        return {
+          id: user.id,
+          email: user.email,
+          username: user.username,
+        };
+      },
+    }),
+  ],
+});
