ba4579ae83855d86b7d8e74260206e8a75930f25
Dorota Boinska <dboinska@gmail.com>
2026-05-28 12:24:00 +0200

    chore: add root layout and cn utility

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`h-full antialiased`}>
-      <body className="min-h-full flex flex-col bg-slate-900 text-white">{children}</body>
+      <body className="min-h-full flex flex-col bg-slate-900 text-white">
+        {children}
+      </body>
     </html>
   );
 }
